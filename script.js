f6b6dc21d38c0d57c1cc3e08a81fb261f0d80b35
lapekka11 <masierolapo@gmail.com>
2025-09-13 16:07:21 +0200

    frontEnd Finished

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const headSlider = document.getElementById('headSlider');
 const eyeSlider = document.getElementById('eyeSlider');
-const mouthSlider = document.getElementById('mouthSlider');
 const torsoSlider = document.getElementById('torsoSlider');
-const armSlider = document.getElementById('armSlider');
-const legSlider = document.getElementById('legSlider');
-const footSlider = document.getElementById('footSlider');
+const eyeColour = document.getElementById('eyeColour');
+const outfitColour = document.getElementById('outfitColour');
 
 const headText = document.getElementById('headText');
 const eyesText = document.getElementById('eyesText');
@@ -14,41 +12,49 @@ const armText = document.getElementById('armText');
 const legsText = document.getElementById('legsText');
 const footText = document.getElementById('footText');
 
+const head = document.getElementById('headImg');
+const eyes = document.getElementById('eyeImg');
+const torso = document.getElementById('torsoimg');
+const leg1 = document.getElementById('leg1');
+const leg2 = document.getElementById('leg2');
+
+
+const randomGenButton = document.getElementById("")
 
 
 
 headSlider.addEventListener('input', function(event) {
     console.log('Slider value:', event.target.value);
     headText.innerHTML="Head: " + event.target.value;
+    head.src = "images/head" + event.target.value + ".png";
+    
 });
 
 eyeSlider.addEventListener('input', function(event) {
     console.log('Slider value:', event.target.value);
     eyesText.innerHTML="Eyes: " + event.target.value;
+    eyes.src = "images/eye" + event.target.value + ".png";
 });
 
-mouthSlider.addEventListener('input', function(event) {
-    console.log('Slider value:', event.target.value);
-    mouthText.innerHTML="Mouth: " + event.target.value;
-});
 
 torsoSlider.addEventListener('input', function(event) {
     console.log('Slider value:', event.target.value);
     torsoText.innerHTML="Torso: " + event.target.value;
+    torso.src = "images/torso" + event.target.value + ".png";
 });
 
-armSlider.addEventListener('input', function(event) {
+eyeColour.addEventListener('input', function(event) {
     console.log('Slider value:', event.target.value);
-    armText.innerHTML="Arms: " + event.target.value;
+    eyeColourText.innerHTML="Eye colour: " + event.target.value;
+    eyes.style.filter = "hue-rotate(" + event.target.value + "deg)"
 });
 
-legSlider.addEventListener('input', function(event) {
+outfitColour.addEventListener('input', function(event) {
     console.log('Slider value:', event.target.value);
-    legsText.innerHTML="Legs: " + event.target.value;
-});
+    outfitColourText.innerHTML="Outfit RGB: " + event.target.value;
+    torso.style.filter = "hue-rotate(" + event.target.value + "deg)"
+    leg1.style.filter = "hue-rotate(" + event.target.value + "deg)"
+    leg2.style.filter = "hue-rotate(" + event.target.value + "deg)"
 
-footSlider.addEventListener('input', function(event) {
-    console.log('Slider value:', event.target.value);
-    footText.innerHTML="Feet: " + event.target.value;
 });
 
